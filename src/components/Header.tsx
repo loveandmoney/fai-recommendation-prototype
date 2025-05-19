f2af54b504baf65bde88fefe2fc4c0fa8597a692
flangham <finnian.langham@gmail.com>
2025-05-19 13:10:49 +1000

    install posthog

---
+++ src/components/Header.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
+
+export const Header = () => {
+  const pathname = usePathname();
+
+  return (
+    <header>
+      <nav className="flex gap-4 p-4">
+        <Link className={clsx(pathname === '/' && 'bg-amber-800')} href="/">
+          Home
+        </Link>
+        <Link
+          className={clsx(pathname === '/about' && 'bg-amber-800')}
+          href="/about"
+        >
+          About
+        </Link>
+        <Link
+          className={clsx(pathname === '/contact' && 'bg-amber-800')}
+          href="/contact"
+        >
+          Contact
+        </Link>
+      </nav>
+    </header>
+  );
+};
